0878f352eda4ac7a96015eed5b8f95d5f0809a35
barry-thierno <thsaidbarry@gmail.com>
2022-02-18 19:32:11 +0100

    feat(example): ajout des services

--- outils-notions-genie-logiciel-niveau-l2/examples/pattern-mvc-node/src/app.module.ts
+++ outils-notions-genie-logiciel-niveau-l2/examples/pattern-mvc-node/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule, TypeOrmModule.forRoot()],
+  imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- outils-notions-genie-logiciel-niveau-l2/examples/pattern-mvc-node/src/products/products.service.ts
+++ outils-notions-genie-logiciel-niveau-l2/examples/pattern-mvc-node/src/products/products.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productsRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.productsRepository.findOne(id);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: Partial<UpdateProductDto>) {
+    await this.productsRepository.update({ id }, updateProductDto);
+    return this.productsRepository.findOne({ id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.productsRepository.delete(id);
   }
 }
